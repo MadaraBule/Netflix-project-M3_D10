7c472535f230209a3d7f2e385dcb34db560963aa
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-18 12:07:32 +0300

    a

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const movies = {
     method: "GET",
     headers: {
         method: 'PUT',
-        body: JSON.stringify(myObject),
+        body: JSON.stringify(),
         "Content-Type": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmU1Y2E5MDIzOTAwMTVkOTY1ZDIiLCJpYXQiOjE2NTAyNjQ3NzQsImV4cCI6MTY1MTQ3NDM3NH0.KBcELRW-ntNpcDf0tRuqpIpFhAceyHT_CCZ1KQa9Kjo"
     }
