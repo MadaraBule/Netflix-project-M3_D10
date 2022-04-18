c2fb9c69ac66632968d541ed87f6e799635f9023
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-18 11:00:39 +0300

    a

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const movies = {
+    method: "GET",
+    headers: {
+        method: 'PUT',
+        body: JSON.stringify(myObject),
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmU1Y2E5MDIzOTAwMTVkOTY1ZDIiLCJpYXQiOjE2NTAyNjQ3NzQsImV4cCI6MTY1MTQ3NDM3NH0.KBcELRW-ntNpcDf0tRuqpIpFhAceyHT_CCZ1KQa9Kjo"
+    }
+};
+window.onload = () => {
+    fetch(
+        "https://striveschool-api.herokuapp.com/api/movies/",
+        movies
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        });
+};
