b9ca04f2fc160a714c4e2b1fc7e759d5dd982708
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-18 19:34:54 +0300

    lost

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-window.onload = () => {
+const getmovies = () => {
     fetch(
         "https://striveschool-api.herokuapp.com/api/movies/", {
         headers: {
@@ -14,55 +14,60 @@ window.onload = () => {
             console.log(data);
         });
 };
-const handleSubmit = async event => {
-    event.preventDefault()
-
-    const myEvent = {
-        name: document.getElementById('name').value,
-        description: document.getElementById('description').value,
-        category: document.getElementById('category').value,
-        image: document.getElementById('image').value
-    }
-    const response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/123`, {
-        method: 'POST',
-        body: JSON.stringify(myEvent),
-        headers: {
-            'Content-Type': 'application/json'
-        },
+const handleSubmit = () => {
+    document.getElementById('form').addEventListener('submit', e => {
+        e.preventDefault()
     })
-    if (response.ok) {
-
-        // isLoading(false)
-
-        const body = await response.json()
-
-        alert("the new generated ID is: " + body._id)
-
-        console.log(response)
-    }
+}
+const putmovies = async () => {
     try {
+        const myEvent = {
+            name: document.getElementById('name').value,
+            description: document.getElementById('description').value,
+            category: document.getElementById('category').value,
+            image: document.getElementById('image').value
+        }
+        const response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/123`, {
+            method: 'POST',
+            body: JSON.stringify(myEvent),
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmU1Y2E5MDIzOTAwMTVkOTY1ZDIiLCJpYXQiOjE2NTAyNjQ3NzQsImV4cCI6MTY1MTQ3NDM3NH0.KBcELRW-ntNpcDf0tRuqpIpFhAceyHT_CCZ1KQa9Kjo"
 
-    } catch (err) {
-        alert(err)
-    }
+            },
+        })
+        if (response.ok) {
 
-    let moviess = response.json();
-    console.log(moviess);
+            // isLoading(false)
 
-    let row = document.querySelector('index.html/tbody/putHere');
+            const body = await response.json()
 
-    let col = document.createElement('div');
-    col.className = "col-12 col-md-4 mb-5 image-fluid";
-    col.innerHTML = `<div class="card">
+            alert("the new generated ID is: " + body._id)
+
+            console.log(response)
+        }
+        try {
+
+        } catch (err) {
+            alert(err)
+        }
+
+        let moviess = response.json();
+        console.log(moviess);
+
+        let row = document.querySelector('index.html/tbody/putHere');
+
+        let col = document.createElement('div');
+        col.className = "col-12 col-md-4 mb-5 image-fluid";
+        col.innerHTML = `<div class="card">
           <img src=${moviess.imageUrl} class="card-img-top" height="300" alt="..." />
           <div class="card-body">
         </div>
         </div>`;
 
-    row.appendChild(col);
-    try {
-
-    } catch (error) {
-        console.log(error);
-    }
-};
+        row.appendChild(col);
+        try {
+            console.log(moviess);
+        } catch (err) {
+            alert(err)
+        }
