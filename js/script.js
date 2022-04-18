7416e633a2df6d77ed0a5e04bd2477f955c8e7ad
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-18 15:54:09 +0300

    empty array

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-const movies = {
-    method: "GET",
-    headers: {
-        method: 'PUT',
-        body: JSON.stringify(),
-        "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmU1Y2E5MDIzOTAwMTVkOTY1ZDIiLCJpYXQiOjE2NTAyNjQ3NzQsImV4cCI6MTY1MTQ3NDM3NH0.KBcELRW-ntNpcDf0tRuqpIpFhAceyHT_CCZ1KQa9Kjo"
-    }
-}
+
 window.onload = () => {
     fetch(
-        "https://striveschool-api.herokuapp.com/api/movies/",
-        movies
-    )
+        "https://striveschool-api.herokuapp.com/api/movies/", {
+        headers: {
+            method: 'PUT',
+            body: JSON.stringify(),
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmU1Y2E5MDIzOTAwMTVkOTY1ZDIiLCJpYXQiOjE2NTAyNjQ3NzQsImV4cCI6MTY1MTQ3NDM3NH0.KBcELRW-ntNpcDf0tRuqpIpFhAceyHT_CCZ1KQa9Kjo"
+        }
+    })
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
